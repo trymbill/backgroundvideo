cd2332797d3d429fbe686e374a77850157a1d948
ccreeves <reeves_chris@ymail.com>
2016-03-19 14:46:42 -0700

    medium capture quality

--- src/ios/backgroundvideo.m
+++ src/ios/backgroundvideo.m
@@ -50,7 +50,7 @@
     
     //Capture session
     session = [[AVCaptureSession alloc] init];
-    [session setSessionPreset:AVCaptureSessionPresetHigh];
+    [session setSessionPreset:AVCaptureSessionPresetMedium];
     
     //Get the front camera and set the capture device
     AVCaptureDevice *inputDevice = [self getCamera: self.camera];
